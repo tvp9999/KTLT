efba7a81bc00edd80add47f1a29257ba9eb9c267
tvp9999 <97007917+tvp9999@users.noreply.github.com>
2024-03-16 16:06:51 +0700

    Add files via upload

---
+++ secured_client.py
@@ -0,0 +1,38 @@
+import socket
+from OpenSSL import SSL
+
+
+def communicate(client_socket):
+    while True:
+        message = input('Client: ')
+
+        if message.lower() == 'quit':
+            client_socket.send(message.encode('utf-8'))
+            break
+        client_socket.send(message.encode('utf-8'))
+
+        msg = client_socket.recv(1024).decode('utf-8')
+        print(f'Server: {msg}')
+
+    client_socket.close()
+
+
+def main():
+    host = '127.0.0.1'
+    port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print('Connected successfully!')
+
+    context = SSL.Context(SSL.TLSv1_2_METHOD)
+
+    ssl_conn = SSL.Connection(context, client_socket)
+    ssl_conn.set_connect_state()
+    ssl_conn.do_handshake()
+
+    communicate(ssl_conn)
+
+
+if __name__ == '__main__':
+    main()
---
+++ secured_server.py
@@ -0,0 +1,47 @@
+import socket
+from OpenSSL import SSL
+
+
+def handle_client(conn):
+    while True:
+        try:
+            msg = conn.recv(1024).decode('utf-8')
+            if msg.lower() == 'quit':
+                print('Client disconnected!')
+                break
+            print(f"Client: {msg}")
+            response = input("Server: ")
+            conn.send(response.encode('utf-8'))
+        except Exception as e:
+            print(str(e))
+            break
+    conn.close()
+
+
+def main():
+    host = '127.0.0.1'
+    port = 12345
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Server is waiting on {host}:{port}")
+
+    conn, addr = server_socket.accept()
+    print(f"Connected by {addr[0]}:{addr[1]}")
+
+    context = SSL.Context(SSL.TLSv1_2_METHOD)
+    context.use_privatekey_file('private.key')
+    context.use_certificate_file('cert.pem')
+
+    ssl_conn = SSL.Connection(context, conn)
+    ssl_conn.set_accept_state()
+    ssl_conn.do_handshake()
+
+    handle_client(ssl_conn)
+
+    server_socket.close()
+
+
+if __name__ == "__main__":
+    main()
