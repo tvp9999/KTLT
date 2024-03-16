43ca09a39209d79009f7873c463e77ebae718317
tvp9999 <97007917+tvp9999@users.noreply.github.com>
2024-03-16 16:08:53 +0700

    add comments to secured.py

--- secured_server.py
+++ secured_server.py
@@ -30,10 +30,12 @@ def main():
     conn, addr = server_socket.accept()
     print(f"Connected by {addr[0]}:{addr[1]}")
 
+    # Tạo context chỉ định version và cung cấp certificate, key 
     context = SSL.Context(SSL.TLSv1_2_METHOD)
     context.use_privatekey_file('private.key')
     context.use_certificate_file('cert.pem')
 
+    # Đóng gói ssl với SSL/TLS
     ssl_conn = SSL.Connection(context, conn)
     ssl_conn.set_accept_state()
     ssl_conn.do_handshake()
